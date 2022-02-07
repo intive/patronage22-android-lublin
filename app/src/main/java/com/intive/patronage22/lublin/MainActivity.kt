4ad20dc7c469638d9f0e6c2abfc0c4a6ab444281
Rafal Depta <rafal.depta@vimncontractor.com>
2022-02-07 20:50:39 +0100

    Add detekt

--- app/src/main/java/com/intive/patronage22/lublin/MainActivity.kt
+++ app/src/main/java/com/intive/patronage22/lublin/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.intive.patronage22.lublin
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.intive.patronage22.lublin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
