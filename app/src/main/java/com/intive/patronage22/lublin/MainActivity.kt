17efefc1f2b70aa8887dcc4bea849b17ff0513ef
Michał Fiuk <majkel785@gmail.com>
2022-02-09 19:12:50 +0100

    P2022-672 Create App Toolbar

--- app/src/main/java/com/intive/patronage22/lublin/MainActivity.kt
+++ app/src/main/java/com/intive/patronage22/lublin/MainActivity.kt
@@ -20,5 +20,6 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(findViewById(R.id.AppToolbar))
     }
 }
