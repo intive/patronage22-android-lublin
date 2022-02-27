4439ab653371e65e59d9f7bb73400c8f823925c7
Michał Fiuk <majkel785@gmail.com>
2022-02-27 19:43:34 +0100

    P2022-1332 Create mock repository for categories. Add argument for productList: MutableList<Product> in in consequence of adding it to (Product.kt). In (CategoriesFragment.kt) : 1: Initialize the viewModel responsible for the category logic. 2: Override fun onCreateView for inflating (fragment_categories.xml) and use the adapter to load a list of category buttons. 3: Use the Toast.makeText function to display the message "Not implemented" after clicking the button with a category from the list. Add layout(category_list_row.xml) for the button in the category list. Remove the unnecessary <TextView> and add a <ListView> for the categories in the file (fragment_categories.xml).

--- app/src/main/java/com/intive/patronage22/lublin/screens/categories/CategoriesFragment.kt
+++ app/src/main/java/com/intive/patronage22/lublin/screens/categories/CategoriesFragment.kt
@@ -1,11 +1,64 @@
 package com.intive.patronage22.lublin.screens.categories
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.intive.patronage22.lublin.R
+import com.intive.patronage22.lublin.repository.CategoryRepositoryMock
+import com.intive.patronage22.lublin.repository.ProductRepositoryMock
 import com.intive.patronage22.lublin.repository.model.categories.CategoriesViewModel
 
 class CategoriesFragment : Fragment(R.layout.fragment_categories) {
     private lateinit var viewModel: CategoriesViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = CategoriesViewModel(
+            CategoryRepositoryMock(
+                ProductRepositoryMock(requireContext())
+            )
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_categories, container, false)
+        val categoriesListView = view.findViewById<ListView>(R.id.listCategories)
+        val categories = viewModel.categories.toList()
+//        val categories = (0..100).map {"category-$it"} // check scrolling
+        categoriesListView.adapter = object : BaseAdapter() {
+            override fun getCount() = categories.size
+
+            override fun getItem(position: Int) = categories[position]
+
+            override fun getItemId(position: Int) = position.toLong()
+
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                Log.d("inflating", "row ${categories[position]}")
+                val row = layoutInflater.inflate(R.layout.category_list_row, null)
+                val rowButton = row.findViewById<Button>(R.id.categoriesListButton)
+
+                rowButton.text = categories[position]
+                rowButton.setOnClickListener {
+                    Toast.makeText(requireContext(), "Not Implemented", Toast.LENGTH_SHORT).show()
+                }
+                return row
+            }
+
+        }
+        return view
+    }
 
 
 }
