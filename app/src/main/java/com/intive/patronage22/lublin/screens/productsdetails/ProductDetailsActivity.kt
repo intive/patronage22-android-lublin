96102232bac7e69d52f1b7a708c46040111ef5c8
Michał Fiuk <majkel785@gmail.com>
2022-04-20 11:51:43 +0200

    P2022-630-Create-details-screen-for-given-product Set binding to ProductDetailsActivity.kt

--- app/src/main/java/com/intive/patronage22/lublin/screens/productsdetails/ProductDetailsActivity.kt
+++ app/src/main/java/com/intive/patronage22/lublin/screens/productsdetails/ProductDetailsActivity.kt
@@ -3,13 +3,19 @@ package com.intive.patronage22.lublin.screens.productsdetails
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.intive.patronage22.lublin.R
+import com.intive.patronage22.lublin.databinding.ActivityMainBinding
+import com.intive.patronage22.lublin.databinding.ActivityProductDetailsBinding
 import com.intive.patronage22.lublin.productExtraName
 import com.intive.patronage22.lublin.repository.model.Product
 
 class ProductDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProductDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_details)
+        binding = ActivityProductDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val product: Product? = intent.getParcelableExtra(productExtraName)
     }
 }
