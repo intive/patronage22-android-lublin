4c69cf4e3872427f3017a7ad4e155a6d18560cfd
michalfiuk1 <majkel785@gmail.com>
2022-06-14 13:45:40 +0200

    Added detekt dependencies and generate detekt-baseline.xml and detekt.yml. Added io.gitlab plugin.

--- app/src/test/java/com/intive/patronage22/lublin/ExampleUnitTest.kt
+++ app/src/test/java/com/intive/patronage22/lublin/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package com.intive.patronage22.lublin
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
