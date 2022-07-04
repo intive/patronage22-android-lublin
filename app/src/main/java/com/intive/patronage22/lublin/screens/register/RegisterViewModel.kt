19f4599b88ba7f8179da2941c09ae06004c194c8
michalfiuk1 <majkel785@gmail.com>
2022-07-04 09:16:12 +0200

    P2022-2212 Create dataBind variables that holds response after validation. Show response after changing focus. P2022-2216 Secure the fragment against memory leakage.

--- app/src/main/java/com/intive/patronage22/lublin/screens/register/RegisterViewModel.kt
+++ app/src/main/java/com/intive/patronage22/lublin/screens/register/RegisterViewModel.kt
@@ -1,8 +1,35 @@
 package com.intive.patronage22.lublin.screens.register
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.intive.patronage22.lublin.RegisterFlowValidator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class RegisterViewModel @Inject constructor() : ViewModel()
+class RegisterViewModel @Inject constructor(
+    private val registerFlowValidator: RegisterFlowValidator
+) : ViewModel() {
+
+    private val _usernameValidationResult = MutableLiveData<String>()
+    val usernameValidationResult: LiveData<String> = _usernameValidationResult
+
+    private val _passwordValidationResult = MutableLiveData<String>()
+    val passwordValidationResult: LiveData<String> = _passwordValidationResult
+
+    private val _emailValidationResult = MutableLiveData<String>()
+    val emailValidationResult: LiveData<String> = _emailValidationResult
+
+    fun onUsernameChanged(username: String) {
+        _usernameValidationResult.value = registerFlowValidator.validateUsername(username)
+    }
+
+    fun onPasswordChanged(password: String) {
+        _passwordValidationResult.value = registerFlowValidator.validatePassword(password)
+    }
+
+    fun onEmailChanged(email: String) {
+        _emailValidationResult.value = registerFlowValidator.validateEmail(email)
+    }
+}
